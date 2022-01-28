2d5d5a85091a00797628b5e3030617a9c2f3f28a
sravya539 <93430745+sravya539@users.noreply.github.com>
2022-01-27 22:10:47 -0800

    commit

--- src/PowerFactorReportPage/PowerFactorReport.js
+++ src/PowerFactorReportPage/PowerFactorReport.js
@@ -1,15 +1,66 @@
 import React from 'react';
-import { Stack, Button, CssBaseline, AppBar, Toolbar, Typography, Box, TextField, Grid } from '@mui/material';
+import { Stack, Button, CssBaseline, AppBar, Toolbar, Typography, Box, TextField, Grid, MenuItem } from '@mui/material';
 import Home from '@mui/icons-material/Home';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import { DTR, Feeder, Section, SubStation } from './Dropdown';
+//import { DTR } from './Dropdown';
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
+
 import ResponsiveDateRangePicker from './DateDetails';
 import MaterialChart from './SubmitAppScreen';
-
+import Sections from '../utils/Sections.json';
+import SubStation from '../utils/SubStation.json';
+import Feeder from '../utils/Feeder.json';
+import DTR from '../utils/DTR.json';
 
 export default function SelectTextFields() {
+
+  const [section, setSection] = useState([]);
+  const [subStation, setsubStation] = useState([]);
+  const [newsubsta, setnewsubsta] = useState([]);
+
+  const [feeder, setfeeder] = useState([]);
+  const [newfeeder, setnewfeeder] = useState([]);
+
+  const [dtr, setDTR] = useState([]);
+  const [newDTR, setnewDTR] = useState([]);
+
+
+  const onSections = (e) => {
+    console.log(e.target.value);
+    const substas = SubStation.filter((substa) => {
+      return substa.sec_name === e.target.value;
+    });
+    console.log(substas);
+    setSection(e.target.value);
+    setsubStation(substas);
+  };
+
+
+  const onSubStation = (e) => {
+    console.log(e.target.value);
+    setnewsubsta(e.target.value);
+    const feeders = Feeder.filter((feed) => {
+      return feed.substa_name === e.target.value;
+    });
+    console.log(feeders);
+
+    setfeeder(feeders);
+  }
+  const onFeeder = (e) => {
+    console.log(e.target.value);
+    setnewfeeder(e.target.value);
+    const dtrs = DTR.filter((dtr2) => {
+      return dtr2.Feeder_name === e.target.value;
+    });
+    setDTR(dtrs)
+    console.log(dtrs);
+  }
+  const onDTR = (e) => {
+    e.preventDefault()
+    console.log(e.target.value);
+    setnewDTR(e.target.value);
+  }
 
   const initialValues = {
     Sections: '',
@@ -64,7 +115,7 @@ export default function SelectTextFields() {
         <Toolbar >
           <Box sx={{ width: '100%', maxWidth: 300 }}>
             <Typography variant='h6' gutterBottom component="div"><Home color="black" /><KeyboardArrowRightIcon />Reports</Typography>
-            <Typography variant="h6" gutterBottom component="div">Power Factor Report</Typography>
+            <Typography variant="h6" gutterBottom component="div">Load Profile Report</Typography>
           </Box>
         </Toolbar>
       </AppBar>
@@ -72,77 +123,68 @@ export default function SelectTextFields() {
       <Box sx={{ display: 'flex', p: 1, m: 1, bgcolor: 'background.paper', flexDirection: 'row', }}>
       </Box>
       <Grid container direction="row" justifyContent="center" alignContent="center" >
+
         <TextField
-          id="outlined-select-currency-native"
-          name="Sections"
-          value={values.Sections}
-          onChange={handleInputChange}
+          name="sections"
           select
-          label="Sections:"
-          SelectProps={{
-            native: true,
-          }}
+          fullWidth
+          variant="outlined"
+          color="primary"
+          label="sections"
+          onChange={onSections}
+          value={section}
         >
-          {Section.map((option) => (
-            <option key={option.value} value={option.label}>
-              {option.label}
-            </option>
+          {Sections.map((sec) => (
+            <MenuItem value={sec.name} key={sec.sec_id}>
+              {sec.name}
+            </MenuItem>
           ))}
         </TextField>
         <TextField
-          id="outlined-select-currency-native"
           select
-          required
-          label="Sub Station:"
-          SelectProps={{
-            native: true,
-          }}
-          name="SubStation"
-          value={values.SubStation}
-          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+          color="primary"
+          label="sub station"
+          onChange={onSubStation}
+          value={newsubsta}
         >
-          {SubStation.map((option) => (
-            <option key={option.value} value={option.label}>
-              {option.label}
-            </option>
+          {subStation.map((substa) => (
+            <MenuItem value={substa.name} key={substa.substa_id}>
+              {substa.name}
+            </MenuItem>
           ))}
         </TextField>
       </Grid>
       <Grid container direction="row" justifyContent="center" alignContent="center" >
         <TextField
-          id="outlined-select-currency-native"
           select
-          required
-          label="Feeder:"
-          SelectProps={{
-            native: true,
-          }}
-          name="Feeder"
-          value={values.Feeder}
-          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+          color="primary"
+          label="Feeders"
+          onChange={onFeeder}
+          value={newfeeder}
         >
-          {Feeder.map((option) => (
-            <option key={option.value} value={option.label}>
-              {option.label}
-            </option>
+          {feeder.map((fee) => (
+            <MenuItem value={fee.name} key={fee.Feeder_id}>
+              {fee.name}
+            </MenuItem>
           ))}
         </TextField>
         <TextField
-          id="outlined-select-currency-native"
           select
-          required
-          label="DTR:"
-          SelectProps={{
-            native: true,
-          }}
-          name="DTR"
-          value={values.DTR}
-          onChange={handleInputChange}
+          fullWidth
+          variant="outlined"
+          color="primary"
+          label="DTR"
+          onChange={onDTR}
+          value={newDTR}
         >
-          {DTR.map((option) => (
-            <option key={option.value} value={option.label}>
-              {option.label}
-            </option>
+          {dtr.map((dt) => (
+            <MenuItem value={dt.name} key={dt.DTR_id}>
+              {dt.name}
+            </MenuItem>
           ))}
         </TextField>
       </Grid>
@@ -198,7 +240,7 @@ export default function SelectTextFields() {
         />
       </Grid>
       <Stack spacing={2} direction="row" justifyContent="center">
-        <Button variant="contained" onClick={handleSubmit} >SUBMIT </Button>
+        <Button variant="contained" onClick={handleSubmit} >SUBMIT</Button>
         <Button variant="contained" onClick={clear} >CLEAR</Button>
       </Stack>
       <br />
